1a149ece95af009a526478e4617bea3bd080813f
LINlisen <49199663+LINlisen@users.noreply.github.com>
2022-11-05 00:40:25 +0800

    update

--- hw03/src/main.js
+++ hw03/src/main.js
@@ -48,4 +48,86 @@ document.querySelector('.Sliders').addEventListener('change', e => {
       view.baseY = 0;
     });
   });
-  
+var std_param = ['ParamAngleX', 'ParamAngleY', 'ParamAngleZ', 'ParamEyeLOpen',
+  'ParamEyeLSmile', 'ParamEyeROpen', 'ParamEyeRSmile', 'ParamEyeBallX', 'ParamEyeBallY',
+  'ParamEyeBallForm', 'ParamBrowLY', 'ParamBrowRY', 'ParamBrowLX', 'ParamBrowRX',
+  'ParamBrowLAngle', 'ParamBrowRAngle',
+  'ParamBrowLForm', 'ParamBrowRForm', 'ParamMouthForm', 'ParamMouthOpenY', 'ParamCheek',
+  'ParamBodyAngleX', 'ParamBodyAngleY', 'ParamBodyAngleZ', 'ParamBreath', 'ParamArmLA',
+  'ParamArmRA', 'ParamArmLB', 'ParamArmRB', 'ParamHandL', 'ParamHandR',
+  'ParamHairFront', 'ParamHairSide', 'ParamHairBack', 'ParamHairFluffy', 'ParamShoulderY',
+  'ParamBustX', 'ParamBustY', 'ParamBaseX', 'ParamBaseY'];
+var all_param = []
+function importFile() {
+    var importedFile = document.getElementById('import-file').files[0];
+
+    var reader = new FileReader();
+    reader.onload = function() {
+      var fileContent = JSON.parse(reader.result);
+
+      console.log(fileContent.Parameters[0]['Id'])
+      for (i = 0; i < 42; i++) {
+        if(std_param.includes(fileContent.Parameters[i]['Id']))
+        {
+          document.querySelector('#stdSelect').options.add(new Option(fileContent.Parameters[i]['Id'],i));
+          all_param.push(fileContent.Parameters[i]['Id']);
+        }
+        else 
+        {
+          document.querySelector('#speSelect').options.add(new Option(fileContent.Parameters[i]['Id'],i));
+          all_param.push(fileContent.Parameters[i]['Id']);
+        }
+      }
+    };
+    reader.readAsText(importedFile); 
+}
+
+document.querySelector('#stdSelect').addEventListener('change', function(){
+  let name = document.querySelector('#stdSelect').value;
+  document.querySelector('.StandardName').innerHTML =std_param[name];
+});
+
+document.querySelector('#speSelect').addEventListener('change', function(){
+  let value = document.querySelector('#speSelect').value;
+  document.querySelector('.SpecialName').innerHTML =all_param[value];
+});
+
+document.querySelector('#stdMin').addEventListener('change', (event) => {
+  document.querySelector('#stdSlider').min = document.querySelector('#stdMin').value;
+});
+
+document.querySelector('#stdMax').addEventListener('change', (event) => {
+  document.querySelector('#stdSlider').max = document.querySelector('#stdMax').value;
+});
+
+document.querySelector('#speMin').addEventListener('change', (event) => {
+  document.querySelector('#speSlider').min = document.querySelector('#speMin').value;
+});
+
+document.querySelector('#speMax').addEventListener('change', (event) => {
+  document.querySelector('#speSlider').max = document.querySelector('#speMax').value;
+});
+
+
+document.querySelector('#stdSlider').addEventListener('change', e => {
+  let val = parseFloat(e.target.value);
+  let id = document.querySelector('.StandardName').innerHTML;
+  id = id.replace("Param", "");
+  id = id[0].toLowerCase() + id.slice(1);
+  console.log(val)
+  runModel(view => {
+    view[id] = val;
+  });
+});
+
+document.querySelector('#speSlider').addEventListener('change', e => {
+  let val = parseFloat(e.target.value);
+  let id = document.querySelector('.SpecialName').innerHTML;
+  id = id.replace("Param", "");
+  id = id[0].toLowerCase() + id.slice(1);
+  console.log(val)
+  console.log(id)
+  runModel(view => {
+    view[id] = val;
+  });
+});
